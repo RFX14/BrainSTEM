7246e9b675e5851aea3322cebafb37aeef6b7e28
coffeemates <jrosa1756@gmail.com>
2021-12-14 20:50:26 -0600

    Initial setup, not fully working yet

---
+++ electron/main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    }
+  })
+  win.setMenu(null)
+
+  //load the index.html from a url
+  win.loadURL('http://localhost:3000');
+
+  // Open the DevTools.
+  win.webContents.openDevTools()
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(createWindow)
+
+// Quit when all windows are closed
+app.on('window-all-closed', () => {
+    app.quit()
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
