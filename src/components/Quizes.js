b56492673bbdde0271c4631c7b90492e978699a5
coffeemates <jrosa1756@gmail.com>
2021-12-15 14:24:45 -0600

    Setting up quizes

---
+++ src/components/Quizes.js
@@ -0,0 +1,31 @@
+const testQuiz = {
+    "quizTitle": "Quiz Testing",
+    "quizSynopsis": "This is simply a test of the quiz componenet",
+    "questions": [
+        {
+        "question": "Does this first question look good?",
+        "questionType": "text",
+        "answers": [
+            "Yes",
+            "No"
+        ],
+        "correctAnswer": "1",
+        "messageForCorrectAnswer": "Ayy it looks good",
+        "messageForInorrectAnswer": "sad face",
+        "explanation": "Just checking",
+        "point": "1"
+        },
+        {
+        "question": "Question Two",
+        "questionType": "text",
+        "answers": [
+            "asdf",
+            "asdfasdf",
+            "asdfasdfasdfasd"
+        ],
+        "correctAnswer": "3"
+        }
+    ]
+}
+
+export default testQuiz
