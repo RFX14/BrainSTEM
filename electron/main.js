573d0b15aa6ad828ba1e43014b9d85c4e0a8fc18
coffeemates <jrosa1756@gmail.com>
2021-12-16 13:16:56 -0600

    Electron can msg React with port names

--- electron/main.js
+++ electron/main.js
@@ -1,12 +1,17 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
+const SerialPort = require('serialport')
+
+let win;
 
 function createWindow () {
   // Create the browser window.
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: true,
+      enableRemoteModule: true,
+      contextIsolation: false,
     }
   })
   win.setMenu(null)
@@ -15,13 +20,30 @@ function createWindow () {
   win.loadURL('http://localhost:3000');
 
   // Open the DevTools.
-  //win.webContents.openDevTools()
+  win.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(createWindow)
+
+function listSerial() {
+  if(win) {
+    SerialPort.list()
+      .then((ports) => {
+        console.log(ports[0].path);
+        win.webContents.send('ports', ports[0].path);
+      })
+  } else {
+    console.log('app not ready yet')
+  }
+}
+
+setTimeout(function listPorts() {
+  setTimeout(listPorts, 5000);
+  listSerial();
+}, 5000);
 
 // Quit when all windows are closed
 app.on('window-all-closed', () => {
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,11 @@
 import { Link } from 'react-router-dom'
+const { ipcRenderer } = window.require('electron')
 
 const Header = () => {
+    ipcRenderer.on('ports', (event, msg) => {
+        console.log(msg)
+    })
+
     return (
         <header className='header'>
             <Link to='/'><h1>BrainSTEM</h1></Link>
