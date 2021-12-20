be789d44dc7b35911573dfacb082cd1bc55092f4
coffeemates <jrosa1756@gmail.com>
2021-12-20 14:55:16 -0600

    comm drop down working at a basic level

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,25 @@
 import { Link } from 'react-router-dom'
 import ReactDropdown from 'react-dropdown'
 import './lib/dropdownStyles.css'
+import {useState, useEffect} from 'react'
+
 const { ipcRenderer } = window.require('electron')
 
 const Header = () => {
-    const options = ['one', 'two', 'three']
+    const [ports, updatePorts] = useState([])
+
     ipcRenderer.on('ports', (event, msg) => {
-        console.log(msg)
+        if(msg) {
+            var newArray = []
+            for(const [key, value] of Object.entries(msg)) {
+                console.log(key);
+                newArray.push(value.path);
+            }
+
+            updatePorts(newArray);
+        }
     })
+
     return (
         <div className='container2'>
             <header className='header'>
@@ -16,9 +28,9 @@ const Header = () => {
 
             <h3>Comm Port:</h3>
             <ReactDropdown 
-                options={options}
-                value={options[0]}
-                placeholder={"Select an option"}
+                options={ports}
+                value={ports[0]}
+                placeholder={"Select a port"}
             />
         </div>
     )
