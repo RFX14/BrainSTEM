48dc13a8e7699bbe97ecb04df6ce4ab716da468b
coffeemates <jrosa1756@gmail.com>
2022-01-06 14:04:16 -0600

    Basic Serial charting demo

---
+++ src/components/RealtimeLineChart.js
@@ -0,0 +1,47 @@
+import Chart from 'react-apexcharts';
+
+const RealtimeLineChart = ({dataList, range}) => {
+    const options = {
+        chart: {
+            zoom: {
+                enabled: false
+            },
+            animations: {
+                enabled: true,
+                easing: 'linear',
+                dynamicAnimation: {
+                    speed: 1000
+                }
+            },
+            tooltip: {
+                x: {
+                    format: 'yyyy/MM/dd HH:mm:ss.f'
+                }
+            },
+            xaxis: {
+                type: 'datatime',
+                range: range
+            },
+            yaxis: {
+                labels: {
+                    formatter: val => val.toFixed(0)
+                },
+                title: {
+                    text: 'Value'
+                }
+            },
+            toolbar: {
+                show: false
+            },
+            stroke: {
+                curve: 'smooth'
+            }
+        },
+    };
+    
+    return (
+        <Chart type='line' options={options} series={dataList} />
+    );
+}
+
+export default RealtimeLineChart
