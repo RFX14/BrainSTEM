bbf17d71fa2cbc41ea0bab36678a4df39493d291
coffeemates <jrosa1756@gmail.com>
2022-02-12 19:08:09 -0600

    clean up of arduino code

--- board_code/board_code.ino
+++ board_code/board_code.ino
@@ -2,12 +2,10 @@
 #define STRAINPIN 3
 #define RESISTOR 10000
 
-const int MAIN = 48, THERMO = 49;
-int count = 0;
+const char MAIN = '0', THERMO = '1';
 
 void readMain();
 void readThermo();
-int reset = 1;
 
 void setup() {
   // put your setup code here, to run once:
@@ -16,17 +14,15 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //int incomingByte = THERMO;
-  
-  char incomingByte = (char)Serial.read();
+  char incomingByte = (char) Serial.read();
   Serial.println("Finished Reading");
   
-  if(incomingByte == '0') {
+  if(incomingByte == THERMO) {
     Serial.println(0);
     //Serial.println(analogRead(THERMOPIN));
   } else {
     Serial.println(6);
   }
+  
   delay(500);
 }
