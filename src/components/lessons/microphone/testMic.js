1b759b88aa930f91061c363406b3a39e25acacf9
coffeemates <jrosa1756@gmail.com>
2022-03-29 20:08:13 -0500

    fixed effect issues

--- src/components/lessons/microphone/testMic.js
+++ src/components/lessons/microphone/testMic.js
@@ -10,23 +10,31 @@ const TestMic = () => {
     const [isPlaying, setPlaying] = useState(false);
     const [rawDataArray, setRawDataArray] = useState([]);
 
+    const SAMPLERATE = 512;
+    const LIMIT = SAMPLERATE * 5; // Expected length of 5 secs of audio 
+
     function recordPressed() {
         setRecording(!isRecording); // toggle status
+
+        // Erase array before recording new data
+        if (rawDataArray.length > 0) {
+            setRawDataArray([]);
+        }
     }
 
     function playPressed() {
-        setPlaying(!isPlaying) // toggle status
+        setPlaying(!isPlaying); // toggle status
     }
-/* 
-    if (false) {
-        useEffect(() => {
+
+    // Only used when array isn't full, and button record hasn't been pressed yet
+    useEffect(() => {
+        if (rawDataArray.length < LIMIT && isRecording) {    
             const addData = data => {
                 const newData = ipcRenderer.sendSync('readData', sensor);
                 return [...data, newData];
             }
-        });
-    }
-    */
+        }
+    });
 
     return (
         <div className='container2'>
