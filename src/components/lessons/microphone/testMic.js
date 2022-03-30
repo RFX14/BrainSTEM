62c573cd967e0bcd808a36c76c6dbcc3288f6bd9
coffeemates <jrosa1756@gmail.com>
2022-03-30 09:39:04 -0500

    hopefully this audioBuffer stuff is right....cause fek

--- src/components/lessons/microphone/testMic.js
+++ src/components/lessons/microphone/testMic.js
@@ -4,13 +4,13 @@ import '../../../App.css';
 import RegionsPlugin from "wavesurfer.js/dist/plugin/wavesurfer.regions.min";
 
 const { ipcRenderer } = window.require('electron');
-//var createBuffer = require('audio-buffer-from')
 
 const TestMic = () => {
     const sensor = '5';
     const [isRecording, setRecording] = useState(false);
     const [isPlaying, setPlaying] = useState(false);
     const [rawDataArray, setRawDataArray] = useState([]);
+    const [audioBuffer, setAudioBuffer] = useState();
 
     const SAMPLERATE = 512;
     const LIMIT = SAMPLERATE * 5; // Expected length of 5 secs of audio 
@@ -18,9 +18,38 @@ const TestMic = () => {
     function recordPressed() {
         setRecording(!isRecording); // toggle status
 
-        // Erase array before recording new data
-        if (rawDataArray.length > 0) {
-            setRawDataArray([]);
+        if (isRecording) {
+            // Erase array before recording new data
+            if (rawDataArray.length > 0) {
+                setRawDataArray([]);
+            }
+        } else {
+            var normalRaw = [];
+            let upperRange = 1;
+            let lowerRange = -1;
+            let maxNum = Math.max(rawDataArray);
+            let minNum = Math.min(rawDataArray);
+
+            // Normalize between [-1, 1] because audioBuffer wants that
+            for (var i = 0; i < rawDataArray.length; i++) {
+                let curr = rawDataArray[i];
+                let normCurr = (lowerRange - upperRange) * ((curr - minNum) / (maxNum - minNum)) + lowerRange;
+                normalRaw.push(normCurr);
+            }
+
+            // Create empty audio buffer to fill
+            var audioCtx = new AudioContext();
+            var rawLength = normalRaw.length;
+            var buffer = audioCtx.createBuffer(1, normalRaw, SAMPLERATE);
+
+            // Fill buffer from rawData (hopefully this works)
+            var newBuffer = buffer.getChannelData(0);
+            for (var i = 0; i < newBuffer.length; i++) {
+                // Audio needs to be in [-1.0; 1.0]
+                newBuffer[i] = normalRaw[i];
+            }
+
+            setAudioBuffer(newBuffer);
         }
     }
 
